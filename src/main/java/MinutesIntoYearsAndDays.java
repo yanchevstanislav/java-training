88c1f0a61bf448c009b0a664a87e20fd03fe3991
yanchevstanislav <yanchev.stanislav@gmail.com>
2024-03-04 19:08:40 +0200

    L2-Exercise-6 Adding solution for the sixth exercise

---
+++ src/main/java/MinutesIntoYearsAndDays.java
@@ -0,0 +1,24 @@
+package main.java;
+
+import java.util.Scanner;
+public class MinutesIntoYearsAndDays {
+    public static void main(String[] args) {
+
+        Scanner givenMinutes = new Scanner(System.in);
+
+        System.out.println("Enter the number of minutes:");
+        double minutes = givenMinutes.nextDouble();
+
+        givenMinutes.close();
+
+        double minutesInYear = 365 * 24 * 60;
+        double minutesInDay = 24 * 60;
+
+        double years = minutes / minutesInYear;
+        double remainingMinutes = minutes % minutesInYear;
+
+        double days = remainingMinutes / minutesInDay;
+
+        System.out.println((int) minutes + " minutes is approximately " + (int) years + " years and " + (int) days + " days.");
+    }
+}
