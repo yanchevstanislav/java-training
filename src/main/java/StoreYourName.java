67a49c85df8af57d6d8b667545f4fa8be6835148
yanchevstanislav <yanchev.stanislav@gmail.com>
2024-03-04 18:04:59 +0200

    L2-Exercise-1 Adding solution for the first exercise

---
+++ src/main/java/StoreYourName.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class StoreYourName {
+    public static void main(String[] args) {
+
+        String firstName = "Stanislav";
+        String middleName = "Atanasov";
+        String lastName = "Yanchev";
+
+        System.out.println(firstName + " " + middleName + " " + lastName);
+
+    }
+}
