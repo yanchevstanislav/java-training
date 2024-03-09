4e61b1b47f47e909cabcb38aed74e10e2ff64627
Stanislav Yanchev <161118412+yanchevstanislav@users.noreply.github.com>
2024-03-09 17:20:25 +0200

    L4-Exercise-2 Adding a check for valid input.

--- src/main/java/homweork/l4/EligibleToWork.java
+++ src/main/java/homweork/l4/EligibleToWork.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 public class EligibleToWork {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double age;
 
-        System.out.println("Please enter your age:");
-        double age = scanner.nextDouble();
-
-        if (age >= 18) {
-
-            System.out.println("You are eligible to work!");
-        }else{
-            System.out.println("Sorry, you are not eligible to work.");
+        while (true) {
+            System.out.print("Enter your age: ");
+            if (scanner.hasNextDouble()) {
+                age = scanner.nextDouble();
+                if (age >= 18 && age < 65) {
+                    break;
+                } else {
+                    System.out.println("Sorry! You are not eligible to work.");
+                }
+            } else {
+                System.out.println("Invalid input.");
+                scanner.next();
+            }
         }
+        System.out.println("You are eligible to work.");
     }
 }
