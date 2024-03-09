d4fa58b2f4f881346a8f950c591e1213ad49648a
Stanislav Yanchev <161118412+yanchevstanislav@users.noreply.github.com>
2024-03-09 16:50:12 +0200

    L4-Exercise-1 Adding a check for valid input.

--- src/main/java/homweork/l4/DaysOfTheWeek.java
+++ src/main/java/homweork/l4/DaysOfTheWeek.java
@@ -5,29 +5,38 @@ import java.util.Scanner;
 public class DaysOfTheWeek {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number (1-7): ");
+        int dayNumber;
 
-        int dayNumber = scanner.nextInt();
-
-        if (dayNumber >= 1 && dayNumber <= 7) {
-            String dayOfWeek = switch (dayNumber) {
-                case 1 -> "Sunday";
-                case 2 -> "Monday";
-                case 3 -> "Tuesday";
-                case 4 -> "Wednesday";
-                case 5 -> "Thursday";
-                case 6 -> "Friday";
-                case 7 -> "Saturday";
-                default -> "Unknown day";
-            };
-
-            System.out.println("The " + dayNumber + getSuffix(dayNumber) + " day of the week is " + dayOfWeek);
-        } else {
-            System.out.println("Invalid input. Please enter a number between 1 and 7.");
+        while (true) {
+            System.out.print("Enter a number (1-7): ");
+            if (scanner.hasNextInt()) {
+                dayNumber = scanner.nextInt();
+                if (dayNumber >= 1 && dayNumber <= 7) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a whole number between 1 and 7.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a whole number between 1 and 7.");
+                scanner.next();
+            }
         }
+
+        String dayOfWeek = switch (dayNumber) {
+            case 1 -> "Sunday";
+            case 2 -> "Monday";
+            case 3 -> "Tuesday";
+            case 4 -> "Wednesday";
+            case 5 -> "Thursday";
+            case 6 -> "Friday";
+            case 7 -> "Saturday";
+            default -> "Unknown day";
+        };
+
+        System.out.println("The " + dayNumber + getSuffix(dayNumber) + " day of the week is " + dayOfWeek);
     }
+
     public static String getSuffix(int num) {
         if (num >= 11 && num <= 13) {
             return "th";
