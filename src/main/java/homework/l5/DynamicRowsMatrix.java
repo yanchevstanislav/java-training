6d95062959350c130b9cbfbc7ac9351702c6e336
Stanislav Yanchev <161118412+yanchevstanislav@users.noreply.github.com>
2024-03-16 11:49:31 +0200

    L5-Exercise-3 Adding solution for exercise 3

---
+++ src/main/java/homework/l5/DynamicRowsMatrix.java
@@ -0,0 +1,33 @@
+package main.java.homework.l5;
+
+import java.util.Scanner;
+public class DynamicRowsMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the desired number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the desired number of columns:");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Enter the desired number for position (" + (i + 1) + "," + (j + 1) + "): ");
+                matrix[i][j] = scanner.nextInt();
+            }
+
+        }
+        System.out.println("The matrix is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
+
