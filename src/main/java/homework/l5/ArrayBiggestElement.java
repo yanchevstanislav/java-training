52d73ccfe979bb2555c24df5a8c08e6727ce5398
Stanislav Yanchev <161118412+yanchevstanislav@users.noreply.github.com>
2024-03-16 15:35:09 +0200

    .

--- src/main/java/homework/l5/ArrayBiggestElement.java
+++ src/main/java/homework/l5/ArrayBiggestElement.java
@@ -6,11 +6,11 @@ public class ArrayBiggestElement {
         int[] array = {2,8,23,12,34,11,3};
 
         int biggestNumber =0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > biggestNumber){
-                biggestNumber = array[i];
+        for (int j : array) {
+            if (j > biggestNumber) {
+                biggestNumber = j;
             }
-            
+
         }
         System.out.println("The biggest number is: "+ biggestNumber);
     }
