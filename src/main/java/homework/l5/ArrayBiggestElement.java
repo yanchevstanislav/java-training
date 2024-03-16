e4a608573bd25c9271054b945b82ad81090a51b7
Stanislav Yanchev <161118412+yanchevstanislav@users.noreply.github.com>
2024-03-16 11:54:25 +0200

    L5-Exercise-4 Adding solution for exercise 4

---
+++ src/main/java/homework/l5/ArrayBiggestElement.java
@@ -0,0 +1,19 @@
+package main.java.homework.l5;
+
+import java.sql.SQLOutput;
+
+public class ArrayBiggestElement {
+    public static void main(String[] args) {
+        
+        int[] array = {2,8,23,12,34,11,3};
+
+        int biggestNumber =0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > biggestNumber){
+                biggestNumber = array[i];
+            }
+            
+        }
+        System.out.println("The biggest number is: "+ biggestNumber);
+    }
+}
